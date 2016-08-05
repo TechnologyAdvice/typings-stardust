ab9c883a71fe84fcae7a7fe32c92c169d79c7749
David Zukowski <zukoweb@gmail.com>
2016-08-05 15:43:40 -0400

    feat(project): initial commit

---
+++ test/example.tsx
@@ -0,0 +1,19 @@
+/// <reference path="../stardust.d.ts" />
+import * as React from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+} from '../stardust'
+
+const renderButton = () => (
+  <Button className='hi' />
+)
+
+const renderContainer = () => (
+  <Container textAlign='center' />
+)
+
+const renderDivider = () => (
+  <Divider />
+)
